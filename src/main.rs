c7aef8a2d2896827bb746be0b42acc39931b480b
mramirez <ramirezmike2@gmail.com>
2020-05-07 18:45:07 -0400

    Added orbtk dependency and created hello world window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use orbtk::prelude::*;
+
 fn main() {
     println!("Hello, world!");
+
+    Application::new()
+        .window(|ctx| {
+            Window::create().title("image_organizer")
+                            .position((100.0, 100.0))
+                            .size(420.0, 730.0)
+                            .child(TextBlock::create().text("Hello World").build(ctx))
+                            .build(ctx)
+        }).run();
 }
