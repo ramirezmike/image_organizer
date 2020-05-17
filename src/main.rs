46b1ccc21818cc4ed29958e4f54ecf57b5f6a17d
mramirez <ramirezmike2@gmail.com>
2020-05-17 10:37:30 -0400

    Added function to list folders in a directory

--- src/main.rs
+++ src/main.rs
@@ -71,8 +71,36 @@ impl Template for MainView {
     }
 }
 
+mod file_io {
+    use std::fs;
+    use std::path::Path;
+
+    pub fn get_directory_list(directory_path:&str) -> Result<Vec<String>, std::io::Error> {
+        let mut found_paths: Vec<String> = Vec::new();
+        let path = Path::new(&directory_path);
+
+        for entry in fs::read_dir(path)? {
+            let found_path = entry?.path();
+            if !found_path.is_dir() {
+                if let Some(path) = found_path.to_str() {
+                    found_paths.push(String::from(path));
+                }
+            }
+        }
+
+        Ok(found_paths)
+    }
+}
+
+
 fn main() {
     println!("Hello, world!");
+
+    let paths = file_io::get_directory_list(".");
+    match paths {
+        Ok(p) => p.iter().for_each(|x| println!("{}", &x)),
+        Err(e) => println!("{:?}", e)
+    }
 
     Application::new()
         .window(|build_context| {
