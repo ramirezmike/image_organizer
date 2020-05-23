b15acfe83fda522c8df232204d3002434edea059
mramirez <ramirezmike2@gmail.com>
2020-05-22 21:51:18 -0400

    Keyhandler work; initial side panel work

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use orbtk::prelude::*;
+use std::char;
 
 mod identifiers {
     pub const SCREEN_SIZE_LABEL: &str = "screen_size_label";
@@ -13,10 +14,10 @@ enum Action {
 
 #[derive(Default, AsAny)] // Default required for widget! macro, AsAny require to impl State
 pub struct WindowState {
-    action: Option<Action> // TODO: I suspect there is a bug here where only one "action"
-                           //       can be handled per update loop. If another action
-                           //       occurs before an update iteration, it will likely overwrite
-                           //       the previously queued action.
+    action: Option<Action>, // TODO: I suspect there is a bug here where only one "action"
+                            //       can be handled per update loop. If another action
+                            //       occurs before an update iteration, it will likely overwrite
+                            //       the previously queued action.
 }
 
 impl WindowState {
@@ -39,35 +40,104 @@ impl State for WindowState {
                     let mut child_label = context.child(identifiers::SCREEN_SIZE_LABEL);
                     child_label.set::<String16>("text", String16::from(format!("{}x{}", w, h)));
                     child_label.set::<f64>("font_size", w * 0.15);
+
                 }
             }
 
             // action handled, setting back to None so nothing happens when Update is called again
             self.action = None; 
         }
+
+        let test_mode = AppMode::new(|key| {
+            println!("{:?} pressed", key);
+            if AppMode::is_alpha(key) {
+                println!("Is Alpha");
+            }
+
+            match key {
+                Key::Space => {
+                    println!("Space!");
+                },
+                Key::A(is_capitalized) => {
+                    println!("A! {}", is_capitalized);
+                },
+                _ => ()
+            }
+        });
+        
+        let mut window = context.window();
+        let global = window.get_mut::<Global>("global");
+        let keyboard_state = &mut global.keyboard_state;
+        test_mode.keys.iter().for_each(|key| {
+            if keyboard_state.is_key_down(*key) {
+                (test_mode.handler)(key);
+                keyboard_state.set_key_state(*key, false);
+            }
+        });
     }
 }
 
+struct AppMode {
+    pub handler: fn(&Key),
+    pub keys: Vec<Key>,
+}
+
+impl AppMode {
+    const SPACE_ASCII:u8 = 32;
+    const UPPER_CASE_A_ASCII:u8 = 65;
+    const UPPER_CASE_Z_ASCII:u8 = 90;
+    const LOWER_CASE_A_ASCII:u8 = 97;
+    const LOWER_CASE_Z_ASCII:u8 = 122;
+    pub fn new(f: fn(&Key)) -> AppMode {
+        AppMode { 
+            handler: f,
+            keys: (AppMode::SPACE_ASCII as u32..=AppMode::LOWER_CASE_Z_ASCII as u32)
+                      .map(|x| Key::from(char::from_u32(x).unwrap_or_default()))
+                      .filter(|x| *x != Key::Unknown)
+                      .collect(),
+        }
+    }
+
+    pub fn is_alpha(key: &Key) -> bool {
+        match Option::<u8>::from(*key) {
+            Some(key) => (key >= AppMode::UPPER_CASE_A_ASCII && key <= AppMode::UPPER_CASE_Z_ASCII) 
+                      || (key >= AppMode::LOWER_CASE_A_ASCII && key <= AppMode::LOWER_CASE_Z_ASCII),
+            None => false
+        }
+    }
+}
+
+
 // uses the widget! macro to generate a MainView widget with our WindowState above
-widget!(MainView<WindowState> {});
+widget!(MainView<WindowState>: KeyDownHandler {
+    background: Brush
+});
 
 // template for how the way the MainView widget looks
 impl Template for MainView {
     fn template(self, _id: Entity, build_context: &mut BuildContext) -> Self {
+        let container = Container::create()
+                .background("#bbdd00")
+                .width(200.0)
+//                .height(800.0)
+                .build(build_context);
+
+        build_context.append_child_to_overlay(container).unwrap();
         self.name("MainView")
-            .width(1280.0)
-            .height(800.0)
-            .child(TextBlock::create()
-                             .width(0.0)
-                             .height(14.0)
-                             .font_size(72.0)
-                             .text("Hello World")
-                             .element("text-block")
-                             // this is where we hardcode an identifier to grab this textblock
-                             // in the action handling to update it later
-                             .id(identifiers::SCREEN_SIZE_LABEL) 
-                             .vertical_alignment("start")
-                             .build(build_context))
+            .child(Container::create()
+                    .background("ccccdd")
+                    .child(TextBlock::create()
+                                     .width(0.0)
+                                     .height(14.0)
+                                     .font_size(72.0)
+                                     .text("Hello World")
+                                     .element("text-block")
+                                     // this is where we hardcode an identifier to grab this textblock
+                                     // in the action handling to update it later
+                                     .id(identifiers::SCREEN_SIZE_LABEL) 
+                                     .vertical_alignment("start")
+                                     .build(build_context))
+                    .build(build_context))
     }
 }
 
@@ -102,31 +172,31 @@ fn main() {
         Err(e) => println!("{:?}", e)
     }
 
-    Application::new()
-        .window(|build_context| {
-            // this creates an instance of the MainView widget and adds it to the build context
-            // build returns an "Entity" which is an identifier that we use later
-            // to pull out WindowState
-            let main_view_entity = MainView::create().build(build_context);
-            Window::create().title("image_organizer")
-                            .position((100.0, 100.0))
-                            .size(1280.0, 800.0)
-                            .insert_handler(WindowEventHandler {
-                                // "move" used to keep borrowed main_view_entity alive long enough
-                                handler: Rc::new(move |states_context, e| { 
-                                    // here using if let pattern match again
-                                    if let WindowEvent::Resize { width, height } = e {
-                                        // gets a mutable reference to the WindowState object
-                                        // in the MainView widget and inserts an Action
-                                        // using the resolution we got from the event handler
-                                        states_context.get_mut::<WindowState>(Entity::from(main_view_entity))
-                                                      .action(Action::Resize { width, height });
-                                    }
-                                    true  
-                                })
+    let app = Application::new();
+    app.window(|build_context| {
+        // this creates an instance of the MainView widget and adds it to the build context
+        // build returns an "Entity" which is an identifier that we use later
+        // to pull out WindowState
+        let main_view_entity = MainView::create().build(build_context);
+        Window::create().title("image_organizer")
+                        .position((100.0, 100.0))
+                        .size(1920.0, 800.0)
+                        .insert_handler(WindowEventHandler {
+                            // "move" used to keep borrowed main_view_entity alive long enough
+                            handler: Rc::new(move |states_context, e| { 
+                                // here using if let pattern match again
+                                if let WindowEvent::Resize { width, height } = e {
+                                    // gets a mutable reference to the WindowState object
+                                    // in the MainView widget and inserts an Action
+                                    // using the resolution we got from the event handler
+                                    states_context.get_mut::<WindowState>(Entity::from(main_view_entity))
+                                                  .action(Action::Resize { width, height });
+                                }
+                                true  
                             })
-                            .resizeable(true)
-                            .child(main_view_entity) // this is adding the MainView widget
-                            .build(build_context)
-        }).run();
+                        })
+                        .resizeable(true)
+                        .child(main_view_entity) // this is adding the MainView widget
+                        .build(build_context)
+    }).run();
 }
