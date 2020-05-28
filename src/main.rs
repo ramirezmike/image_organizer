94b732b0eb7de5d50c26b1a43c42749939ab1012
mramirez <ramirezmike2@gmail.com>
2020-05-27 20:39:48 -0400

    Added test image loading and styling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use iced::{ pane_grid, PaneGrid, executor, Command, Scrollable, scrollable, Length,
+            Image, 
             Subscription, Container, Element, Align, Application, Text, Settings };
 use iced_native::{ keyboard, Event };
 
@@ -64,16 +65,17 @@ impl Application for App {
                         match keyboard_event {
                             keyboard::Event::KeyPressed { .. }  => (),
                             keyboard::Event::KeyReleased { .. }  => (),
-                            keyboard::Event::CharacterReceived(c) => {
+                            keyboard::Event::CharacterReceived(character) => {
                                 if let Some(x) = self.state.get_mut(&self.side_panel) {
                                     if let AppView::SidePanel { ref mut label } = &mut x.app_view { 
-                                        *label = label.to_string() + &c.to_string();
+                                        *label = label.to_string() + &character.to_string();
+                                        // TODO handle adding a tag
                                     }
                                 }
 
                                 if let Some(x) = self.state.get_mut(&self.image_display) {
-                                    if let AppView::ImageDisplay { ref mut label } = &mut x.app_view { 
-                                        *label = label.to_string() + &c.to_string() + &String::from("?");
+                                    if let AppView::ImageDisplay { .. } = &mut x.app_view { 
+                                        // TODO handle tagging an image here
                                     }
                                 }
                             }
@@ -106,6 +108,7 @@ impl Application for App {
         Container::new(pane_grid)
             .width(Length::Fill)
             .height(Length::Fill)
+            .style(style::MainWindow { })
             .padding(10)
             .into()
     }
@@ -114,6 +117,9 @@ impl Application for App {
 enum AppView {
     SidePanel {
         label: String
+    },
+    ImageQueue { 
+        // TODO split a pane and implement bottom queue view across entire width 
     },
     ImageDisplay {
         label: String
@@ -143,19 +149,98 @@ impl Content {
                 Container::new(scrollable)
                     .width(Length::Fill)
                     .height(Length::Fill)
-                    .center_y()
+                    .style(style::Pane { })
+                    .center_x()
                     .into()
             },
-            AppView::ImageDisplay { label, .. } => {
+            AppView::ImageQueue { .. } => {
                 let scrollable = Scrollable::new(&mut self.scroll)
-                    .align_items(Align::Start)
-                    .push(Text::new(label.to_string()).size(30));
+                    .align_items(Align::Start);
                     
                 Container::new(scrollable)
                     .width(Length::Fill)
                     .height(Length::Fill)
                     .center_y()
+                    .center_x()
                     .into()
+            },
+            AppView::ImageDisplay { label, .. } => {
+                let scrollable = Scrollable::new(&mut self.scroll)
+                    .align_items(Align::Start)
+                    .push(Text::new(label.to_string()).size(30))
+                    .push(load_image("images/test.jpg".to_string()));
+                    
+                Container::new(scrollable)
+                    .width(Length::Fill)
+                    .height(Length::Fill)
+                    .center_y()
+                    .center_x()
+                    .into()
+            }
+        }
+    }
+}
+
+fn load_image<'a>(src: String) -> Container<'a, Message> {
+    Container::new(
+        // This implementation was based on the "tour" example
+        // https://github.com/hecrj/iced/blob/master/examples/tour/src/main.rs
+        // At the time, it said the following:
+        //
+        // This should go away once we unify resource loading on native platforms
+        //
+        if cfg!(target_arch = "wasm32") {
+            Image::new(src)
+        } else {
+            Image::new(format!("{}/{}", env!("CARGO_MANIFEST_DIR"), src))
+        }
+        .width(Length::Fill)  // TODO: Not sure if it would be handled here or in resize
+        .height(Length::Fill) // but it'd be good to make the images resize correctly when the window
+                              // resizes so it doesn't introduce scrolling (maybe don't put this in a 
+                              // scrollable container?)
+    )
+    .width(Length::Fill)
+    .center_x()
+}
+
+//TODO: move this to another file
+mod style {
+    use iced::{container, Background, Color};
+
+    const BACKGROUND: Color = Color::from_rgb(
+        0x1F as f32 / 255.0,
+        0x24 as f32 / 255.0,
+        0x30 as f32 / 255.0,
+    );
+    const TEXT : Color = Color::from_rgb(
+        0xCB as f32 / 255.0,
+        0xCC as f32 / 255.0,
+        0xC6 as f32 / 255.0,
+    );
+
+    pub struct MainWindow { }
+    impl container::StyleSheet for MainWindow {
+        fn style(&self) -> container::Style {
+            container::Style {
+                text_color: Some(TEXT),
+                background: Some(Background::Color(BACKGROUND)),
+                ..Default::default()
+            }
+        }
+    }
+
+    pub struct Pane { }
+    impl container::StyleSheet for Pane {
+        fn style(&self) -> container::Style {
+            container::Style {
+                text_color: Some(TEXT),
+                background: Some(Background::Color(BACKGROUND)),
+                border_width: 2,
+                border_color: Color {
+                    a: 0.3,
+                    ..Color::BLACK
+                },
+                ..Default::default()
             }
         }
     }
